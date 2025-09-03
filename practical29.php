44f2668fe6bfba6d521bc7d595ef5d09a46ce7de
arpitamaurya0129-ops <arpita.maurya.0129@gmail.com>
2025-09-03 19:36:21 +0530

    Add files via upload

---
+++ practical29.php
@@ -0,0 +1,22 @@
+<?php
+$arr=[10,20,30,40,20,50,10];
+$unique=array_unique($arr);
+echo"Original Array:";
+print_r($arr);
+echo"<br>After Removing Duplicates:";
+print_r($unique);
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ practical30.php
@@ -0,0 +1,6 @@
+<?php
+$countries=["India"=>"New Delhi","USA"=>"Washington DC","Uk"=>"London","France"=>"Paris","Japan"=>"Tokyo"];
+foreach($countries as $country=>$capital){
+    echo"$country 'n $capital<br>";
+}
+?>
